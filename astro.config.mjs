10734e68a812b87af7a17827df27d7c7cf99079e
John Ogletree <pxltr3@gmail.com>
2025-08-06 00:28:22 -0400

    Initial Astro website commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind'; // This line is no longer needed
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [], // The array is now empty
+});
