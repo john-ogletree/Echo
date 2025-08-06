8300374b9706ac3d5d9c5af6a5f7fab1a1e2d674
John Ogletree <pxltr3@gmail.com>
2025-08-06 00:53:54 -0400

    Revert "moved to tailwindcss"
    This reverts commit 92cb6a5ec65955b74e6d5de8a518279864cb2565.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 // import tailwind from '@astrojs/tailwind'; // This line is no longer needed
 
 // https://astro.build/config
 export default defineConfig({
-  // The array is now empty
-  integrations: [],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [], // The array is now empty
 });
