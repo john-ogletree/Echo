611d939785d7dfb333a8eb9054d105c3e74c77ef
john-ogletree <pxltr3@gmail.com>
2025-09-11 17:11:19 -0400

    Auto-commit: file change in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['i18next', 'i18next-browser-languagedetector'],
+      },
+    },
+  },
+});
