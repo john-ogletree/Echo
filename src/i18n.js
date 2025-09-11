c62b134cceafa52072b0d4543685ff270a047c42
john-ogletree <pxltr3@gmail.com>
2025-09-11 17:17:11 -0400

    Auto-commit: file change in i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,32 +1,20 @@
-let i18nInstance;
+import i18n from 'i18next';
 
-export const setupI18n = async (resources, lang) => {
-  if (!i18nInstance) {
-    const i18n = (await import('i18next')).default;
-    const LanguageDetector = (await import('i18next-browser-languagedetector')).default;
-
-    i18nInstance = i18n;
-
-    if (typeof window !== 'undefined') {
-      i18nInstance.use(LanguageDetector);
-    }
-
-    await i18nInstance.init({
+// This function will be called with resources from the Astro component
+export const setupI18n = (resources, lang) => {
+  if (!i18n.isInitialized) {
+    i18n.init({
       resources,
       fallbackLng: 'en',
       lng: lang,
       debug: true,
-      detection: {
-        order: ['navigator', 'htmlTag', 'path', 'subdomain'],
-        caches: ['localStorage'],
-      },
       interpolation: {
         escapeValue: false,
       },
     });
   } else {
-    i18nInstance.addResourceBundle(lang, 'translation', resources[lang].translation, true, true);
-    i18nInstance.changeLanguage(lang);
+    i18n.addResourceBundle(lang, 'translation', resources[lang].translation, true, true);
+    i18n.changeLanguage(lang);
   }
-  return i18nInstance;
+  return i18n;
 };
