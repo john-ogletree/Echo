4eb4de4feb3e9ea5d842be9adce814aa5d1e2491
john-ogletree <pxltr3@gmail.com>
2025-09-11 17:20:38 -0400

    Auto-commit: file change in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        'i18next': path.resolve(__dirname, './node_modules/i18next/dist/esm/i18next.js'),
-        'i18next-browser-languagedetector': path.resolve(__dirname, './node_modules/i18next-browser-languagedetector/dist/esm/i18nextBrowserLanguageDetector.js'),
-      },
-    },
-  },
-});
+export default defineConfig({});
