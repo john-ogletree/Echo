8c4f63e77acc4800d38deebdce68b90cde3939f3
john-ogletree <pxltr3@gmail.com>
2025-09-11 17:17:18 -0400

    Auto-commit: file change in i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,3 @@
 import i18n from 'i18next';
 
-// This function will be called with resources from the Astro component
-export const setupI18n = (resources, lang) => {
-  if (!i18n.isInitialized) {
-    i18n.init({
-      resources,
-      fallbackLng: 'en',
-      lng: lang,
-      debug: true,
-      interpolation: {
-        escapeValue: false,
-      },
-    });
-  } else {
-    i18n.addResourceBundle(lang, 'translation', resources[lang].translation, true, true);
-    i18n.changeLanguage(lang);
-  }
-  return i18n;
-};
+export default i18n;
