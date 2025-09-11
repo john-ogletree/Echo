6ce13dda2a0bc6f6e28785dfdadc211fbb40877e
john-ogletree <pxltr3@gmail.com>
2025-09-11 17:13:50 -0400

    Auto-commit: file change in i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -4,21 +4,25 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // This function will be called with resources from the Astro component
 export const setupI18n = (resources, lang) => {
   if (!i18n.isInitialized) {
-    i18n
-      .use(LanguageDetector)
-      .init({
-        resources,
-        fallbackLng: 'en',
-        lng: lang,
-        debug: true,
-        detection: {
-          order: ['navigator', 'htmlTag', 'path', 'subdomain'],
-          caches: ['localStorage'],
-        },
-        interpolation: {
-          escapeValue: false,
-        },
-      });
+    const i18nInstance = i18n;
+
+    if (typeof window !== 'undefined') {
+      i18nInstance.use(LanguageDetector);
+    }
+
+    i18nInstance.init({
+      resources,
+      fallbackLng: 'en',
+      lng: lang,
+      debug: true,
+      detection: {
+        order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+        caches: ['localStorage'],
+      },
+      interpolation: {
+        escapeValue: false,
+      },
+    });
   } else {
     i18n.addResourceBundle(lang, 'translation', resources[lang].translation, true, true);
     i18n.changeLanguage(lang);
