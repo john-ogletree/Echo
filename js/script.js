65e0aac31da081d18790e0b03b00a3dea0c40442
pxltr3-git <pxltr3@gmail.com>
2025-10-22 04:05:16 -0400

    Automated: Sync changes from main repo to Echo submodule (local script)

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ const body = document.querySelector("body"),
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
   if (sidebar.classList.contains("close")) {
-    toggle.classList.remove("bx-chevron-right");
-    toggle.classList.add("bx-chevron-left");
-  } else {
     toggle.classList.remove("bx-chevron-left");
     toggle.classList.add("bx-chevron-right");
+  } else {
+    toggle.classList.remove("bx-chevron-right");
+    toggle.classList.add("bx-chevron-left");
   }
 });
 
